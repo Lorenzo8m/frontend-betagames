d4b50dc5624e3c491c221a691b0cf8e21f366e87
Matteo-Nardin <123728836+Matteo-Nardin@users.noreply.github.com>
2025-02-25 17:57:53 +0100

    new login

--- src/app/services/subject-service.service.ts
+++ src/app/services/subject-service.service.ts
@@ -1,25 +1,25 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+// import { Injectable } from '@angular/core';
+// import { BehaviorSubject } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class SubjectServiceService {
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class SubjectServiceService {
 
-  qoute = new  BehaviorSubject <{ data:{
-    id:number;
-    carts:{id:number}
-  }[] }>( { data:[]} );
+//   qoute = new  BehaviorSubject <{ data:{
+//     id:number;
+//     carts:{id:number}
+//   }[] }>( { data:[]} );
 
-  currenteQuote = this.qoute.asObservable();
+//   currenteQuote = this.qoute.asObservable();
 
-  getValueCurrentQuote(){
-    return this.qoute.getValue()
-  }
+//   getValueCurrentQuote(){
+//     return this.qoute.getValue()
+//   }
 
-  updateQuote ( newQuote: {data:[]} ){ 
-    this . qoute . next (newQuote); 
-  } 
+//   updateQuote ( newQuote: {data:[]} ){ 
+//     this . qoute . next (newQuote); 
+//   } 
 
-  constructor() { }
-}
+//   constructor() { }
+// }
